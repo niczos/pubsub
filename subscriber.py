ef35f1e471470d9f948c353354435d279a633ee2
niczos <nika.jurczuk@gmail.com>
2021-08-03 12:54:31 +0000

    write to test.log

--- subscriber.py
+++ subscriber.py
@@ -10,6 +10,9 @@ credentials_path = '/home/nika_jurczuk/dataflow-pubsub-key.json'
 subscription_path = 'projects/dataflow-poc-317213/subscriptions/demo-sub'
 # subscription_path = '<PATH_TO_SUBSCRIPTION>'
 
+logging.basicConfig(filename='test.log', level=logging.INFO,
+    format='%(asctime)s:%(levelname)s:%(message)s')
+
 try:
     # set environmental variable
     os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = credentials_path
@@ -17,8 +20,8 @@ try:
 except NameError:
     print('Variable does not exist')
 
-# set timeout to 15s
-timeout = 15
+# set timeout to 8s
+timeout = 8
 
 subscriber = pubsub_v1.SubscriberClient()
 
@@ -27,7 +30,7 @@ def callback(message):
     print(f'Received message: {message}')
     print(f'data: {message.data}')
     logging.info(message)
-    with open('output.txt', 'w') as f:
+    with open('output.txt', 'a') as f:
         f.write(f'{message.data}')
         f.close()
     message.ack()  # delete message from servers
@@ -36,8 +39,6 @@ def callback(message):
 logger = logging.getLogger()
 # change IP to your own public IP of the vm
 logger.addHandler(SysLogHandler(address=('34.118.32.203', 514)))
-logging.basicConfig(filename='test.log', level=logging.INFO,
-    format='%(asctime)s:%(levelname)s:%(message)s')
 
 streaming_pull_future = subscriber.subscribe(subscription_path, callback=callback)
 print(f'listening for message on {subscription_path}')
@@ -48,9 +49,9 @@ with subscriber:
     try:
         streaming_pull_future.result(timeout=timeout)
     except TimeoutError:
-        with open('output.txt', 'w') as f:
-            f.write('Loading failed due to Timeout')
-            f.close()
+  #      with open('output.txt', 'w') as f:
+   #         f.write('Loading failed due to Timeout')
+    #        f.close()
         streaming_pull_future.cancel()  # trigger the shutdown
         streaming_pull_future.result()  # block until shutdown is complete
     
